07934c696415bbfba73631abbb044bb1e6bf4862
stanBrusse <60739397+stanBrusse@users.noreply.github.com>
2020-05-06 10:10:27 +0200

    Update DatabaseConnectie.php

--- project_periode4/DatabaseConnectie.php
+++ project_periode4/DatabaseConnectie.php
@@ -1,4 +1,5 @@
 <?php
+/*
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
@@ -8,5 +9,17 @@ define("DB_NAME", "Equipodo");
 $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 if($conn->connect_error){
+    die("connection failed " . $conn->connect_error);
+}
+*/
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "nbolt");
+define("DB_PASS", "equipodo123");
+define("DB_NAME", "nbolt_equipodo");
+
+$conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
     die("connection failed " . $conn->connect_error);
 }
