f8e9d6bd941997110a494b3b603260f14943956f
Nander <nander.bolt17@gmail.com>
2020-05-11 09:10:55 +0200

    Deleted gamepage, added game into navbar

--- project_periode4/header.php
+++ project_periode4/header.php
@@ -25,7 +25,7 @@
         <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
         <a href="homepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
         <a href="vragenlijst.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Vragenlijst</a>
-        <a href="gamepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Gamepage</a>
+        <a href="https://www.greenfoot.org/scenarios/25826" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Corona game</a>
     </div>
 
     <!-- Navbar on small screens -->
