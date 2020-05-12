bfccc254eccc2759359f0f6694d1368e8885bc76
simonHEH <simonkhb2002@gmail.com>
2020-05-12 09:08:10 +0200

    ...

--- project_periode4/Aantalbesmetting.php
+++ project_periode4/Aantalbesmetting.php
@@ -47,6 +47,47 @@ if($p == 1)
         </div>
 
         <?php
+    }elseif($aantal = 1){
+        $leeftijd = $_POST["leeftijd"];
+
+        $sql = "select  *  FROM gegevens WHERE (plaats  = '" . $plaats . "' AND  besmet='1' AND Leeftijd='" . $leeftijd . "')  ";
+        $result_sql = mysqli_query($conn, $sql);
+        $conn->query($sql);
+
+        $aantal = 0;
+
+        while ($subject1 = mysqli_fetch_assoc($result_sql)) {
+            $aantal++;
+        }
+        $conn->close();
+        ?>
+
+
+        <div class="w3-row-padding w3-padding-64 w3-container">
+            <div class="w3-content">
+                <div class="w3-twothird">
+
+
+
+                    <h4 class="w3-padding-32">er is <?php echo $aantal ?> aantal besmettingen in de
+                        plaats <?php echo $plaats ?>
+                        waarbij de leeftijd <?php echo $leeftijd ?> is.</h4>
+
+
+                </div>
+
+                <div class="w3-third w3-center">
+                    <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+                </div>
+            </div>
+        </div>
+
+       
+        <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+            <h1 class="w3-margin w3-xlarge">Blijf binnen</h1>
+        </div>
+
+        <?php
     }else
         {
             $sql = "select  *  FROM gegevens WHERE (plaats  = '" . $plaats . "' AND  besmet='1' )  ";
