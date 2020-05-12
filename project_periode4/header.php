56ecd93e8110b06113f9d37a1383f84211c141b9
Nander <nander.bolt17@gmail.com>
2020-05-12 09:44:49 +0200

    changes

--- project_periode4/header.php
+++ project_periode4/header.php
@@ -24,13 +24,13 @@
     <div class="w3-bar w3-red w3-card w3-left-align w3-large">
         <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
         <a href="homepage.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-        <a href="vragenlijst.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">vragenlijst</a>
-        <a href="gamepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">games</a>
+        <a href="vragenlijst.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Vragenlijst</a>
+        <a href="gamepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Games</a>
     </div>
 
     <!-- Navbar on small screens -->
     <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-        <a href="vragenlijst.php" class="w3-bar-item w3-button w3-padding-large">vragenlijst</a>
-        <a href="gamepage.php" class="w3-bar-item w3-button w3-padding-large">games</a>
+        <a href="vragenlijst.php" class="w3-bar-item w3-button w3-padding-large">Vragenlijst</a>
+        <a href="gamepage.php" class="w3-bar-item w3-button w3-padding-large">Games</a>
     </div>
 </div>
--- project_periode4/vragenlijst.php
+++ project_periode4/vragenlijst.php
@@ -1,33 +1,33 @@
 <?php
-require "header.php";
+require 'header.php';
 ?>
 <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
     <div class="w3-content">
 
         <div class="w3-twothird">
 
-        <form action="antwoordenVragen.php" class="w3-row-padding w3-light-grey w3-padding-64 w3-container" method="post">
-                Moet je vaak hoesten?                       <input type="checkbox" value="1" name="hoesten"> <br>
-                Heb je koorts?                              <input type="checkbox" value="1" name="koorts"> <br>
-                Heb je spierpijn?                           <input type="checkbox" value="1" name="spier"><br>
-                Heb je hoofdpijn?                           <input type="checkbox" value="1" name="hoofdpijn"><br>
-                Heb je keelpijn?                            <input type="checkbox" value="1" name="keelpijn"><br>
-                Heb je rillingen?                           <input type="checkbox" value="1" name="rillingen"><br>
-                Heb je dat smaak of geur wegvalt?           <input type="checkbox" value="1" name="smaakGeur"><br>
-                Heb je moeite met ademen?                   <input type="checkbox" value="2" name="moeiteAdemen"><br>
-                Heb je konstante pijn op je borst?          <input type="checkbox" value="2" name="borstpijn"><br>
-                Heb je blauwachtige lippen of gezicht?      <input type="checkbox" value="2" name="lippen"><br>
-                <br>
-                Heb je een andere ziekte                    <input type="checkbox" value="true" name="andereZiekte"><br>
-                Woonplaats                                  <input type="text" name="plaats" required><br>
-                Gemeente                                    <input type="text" name="gemeente" required><br>
-                Provincie                                   <input type="text" name="provincie" required><br>
-                Leeftijd                                    <input type="number" name="leeftijd" required><br>
-            <input type="submit">
-        </form>
-</div></div></div>
+            <form action="antwoordenVragen.php" class="w3-row-padding w3-light-grey w3-padding-64 w3-container" method="post">
+                    Moet je vaak hoesten?                       <input type="checkbox" value="1" name="hoesten"><br>
+                    Heb je koorts?                              <input type="checkbox" value="1" name="koorts"><br>
+                    Heb je spierpijn?                           <input type="checkbox" value="1" name="spier"><br>
+                    Heb je hoofdpijn?                           <input type="checkbox" value="1" name="hoofdpijn"><br>
+                    Heb je keelpijn?                            <input type="checkbox" value="1" name="keelpijn"><br>
+                    Heb je rillingen?                           <input type="checkbox" value="1" name="rillingen"><br>
+                    Heb je dat smaak of geur wegvalt?           <input type="checkbox" value="1" name="smaakGeur"><br>
+                    Heb je moeite met ademen?                   <input type="checkbox" value="2" name="moeiteAdemen"><br>
+                    Heb je konstante pijn op je borst?          <input type="checkbox" value="2" name="borstpijn"><br>
+                    Heb je blauwachtige lippen of gezicht?      <input type="checkbox" value="2" name="lippen"><br>
+                    Heb je een andere ziekte                    <input type="checkbox" value="true" name="andereZiekte"><br><br>
+                    Woonplaats                                  <input type="text" name="plaats" required><br>
+                    Gemeente                                    <input type="text" name="gemeente" required><br>
+                    Provincie                                   <input type="text" name="provincie" required><br>
+                    Leeftijd                                    <input type="number" name="leeftijd" required><br>
 
-
+                <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Verzenden</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php
 require "footer.php";
