4b5143820d0d9e905896859aecc6290924699a3d
Nander <nander.bolt17@gmail.com>
2020-05-13 09:02:59 +0200

    header

--- project_periode4/header.php
+++ project_periode4/header.php
@@ -26,10 +26,7 @@
         <a href="homepage.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
         <a href="vragenlijst.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Vragenlijst</a>
         <a href="gamepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Games</a>
-        <a href="homepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
-        <a href="vragenlijst.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">vragenlijst</a>
-        <a href="gamepage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">games</a>
-        <a href="forum.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">forum</a>
+        <a href="forum.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Forum</a>
         <img src="images/default.png" height="60" width="103" style="float:right">
 
     </div>
